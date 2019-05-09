bc6e2323dae0c48904b7c243a4ae1f22fa3fc5b2
Rick Nitsche <rick@phas.ubc.ca>
2019-05-08 17:27:43 -0700

    fix(__init__): only import Broker for python>2
    The broker uses asyncio, which does not exist in python2. For python2
    scripts to use the manager, make the broker import in __init__ depending
    on the python (major) version.

--- comet/__init__.py
+++ comet/__init__.py
@@ -1,4 +1,9 @@
 """CoMeT: A Config and Metadata Tracker."""
 from .version import __version__
-from .broker import Broker
 from .manager import Manager, ManagerError, BrokerError, CometError
+
+# The broker uses asyncio which doesn't exist in python2. However we want to be able to use the
+# manager in python2, so make the Broker import dependent on the python version.
+from sys import version_info
+if version_info.major > 2:
+    from .broker import Broker
