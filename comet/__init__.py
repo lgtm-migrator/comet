10e1f230892af4f2afad28be150be3f6ffc871ed
Rick Nitsche <rick@phas.ubc.ca>
2019-05-21 18:19:41 -0700

    fix(__init__): only import Dumper for python>2
    The Dumper uses asyncio, which does not exist in python2. For python2
    scripts to use the manager, make the import in __init__ depending
    on the python (major) version.

--- comet/__init__.py
+++ comet/__init__.py
@@ -1,10 +1,10 @@
 """CoMeT: A Config and Metadata Tracker."""
 from .version import __version__
 from .manager import Manager, ManagerError, BrokerError, CometError
-from .dumper import Dumper
 
 # The broker uses asyncio which doesn't exist in python2. However we want to be able to use the
 # manager in python2, so make the Broker import dependent on the python version.
 from sys import version_info
 if version_info.major > 2:
     from .broker import Broker
+    from .dumper import Dumper
