c2ce520abc28c49d0ce6657c7399564384aae311
Rick Nitsche <rick@phas.ubc.ca>
2019-10-01 13:28:53 -0700

    fix(redis_)cond_var): fix algorithm
    - add test
    - add pseudocode in comments

--- comet/redis_cond_variable.py
+++ comet/redis_cond_variable.py
@@ -14,17 +14,34 @@ async def redis_condition_wait(redis, name):
     name = "cond_{}".format(name)
 
     # register as a waiting process
+    #
+    # PSEUDOCODE:
+    #
+    # waiting = dict()
+    # waiting[name] += 1
     await redis.execute("hincrby", "WAITING", name, 1)
 
     # Wait for notification
-    await redis.execute("blpop", name)
+    #
+    # PSEUDOCODE:
+    #
+    # while(True):
+    #     if name:
+    #         name = None
+    await redis.execute("blpop", name, 0)
 
     # Decrement number of waiting processes and reset notification.
     # Script that decrements WAITING/KEY[0] and sets KEY[0] to zero if WAITING/KEY[0] is zero:
+    #
+    # PSEUDOCODE:
+    #
+    # waiting[name] -= 1
+    # if waiting[name] > 0:
+    #     name = 1
     REDIS_RESET_COND = """
-    redis.call('hincrby', 'WAITING', KEY[0], -1)
-    if redis.call('hget', 'WAITING', 'KEY[0]) == 0
-        redis.call('set', KEY[0], 0)
+    if redis.call('hincrby', 'WAITING', KEYS[1], -1) ~= 0 then
+        redis.call('lpush', KEYS[1], "1")
+    end
     """
     await redis.execute("eval", REDIS_RESET_COND, 1, name)
 
@@ -40,4 +57,8 @@ async def redis_condition_notify(redis, name):
     """
     name = "cond_{}".format(name)
 
+    #
+    # PSEUDOCODE:
+    #
+    # name = 1
     return await redis.execute("lpush", name, "1")
---
+++ tests/test_redis_cond_var.py
@@ -0,0 +1,85 @@
+import asyncio
+import aioredis
+import pytest
+
+from comet.redis_cond_variable import redis_condition_wait, redis_condition_notify
+
+
+async def wait(name):
+    red = await aioredis.create_redis(
+        ("127.0.0.1", 6379), encoding="utf-8"
+    )
+    await redis_condition_wait(red, name)
+    red.close()
+    await red.wait_closed()
+    del red
+
+
+async def notify(name):
+    red = await aioredis.create_redis(
+        ("127.0.0.1", 6379), encoding="utf-8"
+    )
+    await redis_condition_notify(red, name)
+    red.close()
+    await red.wait_closed()
+    del red
+
+
+@pytest.mark.asyncio
+async def test_cond_variable():
+    name = "a"
+
+    waiter = list()
+    for i in range(20):
+        waiter.append(asyncio.create_task(wait(name)))
+    await asyncio.sleep(1)
+    for w in waiter:
+        assert w.done() is False
+
+    await notify(name)
+
+    for w in waiter:
+        done, pending = await asyncio.wait({w})
+        assert w in done
+        assert w.done() is True
+        await w
+
+@pytest.mark.asyncio
+async def test_cond_two_variables():
+    name_a = "a"
+    name_b = "b"
+
+    waiter_a = list()
+    for i in range(20):
+        waiter_a.append(asyncio.create_task(wait(name_a)))
+
+    waiter_b = list()
+    for i in range(20):
+        waiter_b.append(asyncio.create_task(wait(name_b)))
+
+    await asyncio.sleep(1)
+
+    for w in waiter_a:
+        assert w.done() is False
+
+    await notify(name_b)
+
+    for w in waiter_a:
+        assert w.done() is False
+
+    for w in waiter_b:
+        done, pending = await asyncio.wait({w})
+        assert w in done
+        assert w.done() is True
+        await w
+
+    for w in waiter_a:
+        assert w.done() is False
+
+    await notify(name_a)
+
+    for w in waiter_a:
+        done, pending = await asyncio.wait({w})
+        assert w in done
+        assert w.done() is True
+        await w
