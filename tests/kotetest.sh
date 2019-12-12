8b4f40c420691b245e50845e12abb06b9b44082d
Rick Nitsche <rick@phas.ubc.ca>
2019-12-12 12:08:39 -0800

    test(kotetest): print cpuinfo

--- tests/kotetest.sh
+++ tests/kotetest.sh
@@ -3,10 +3,14 @@
 # exit when any command fails
 set -e
 
+# TODO: switch back to branch develop
 git clone https://github.com/kotekan/kotekan.git --branch rn/fixtests --single-branch
 cd kotekan/build
 cmake -DBOOST_TESTS=ON ..
 make dataset_broker_producer dataset_broker_producer2 dataset_broker_consumer
+
+more /proc/cpuinfo | grep flags
+
 cd ../tests
 export PYTHONPATH=../python:$PYTHONPATH
 pytest -xs test_dataset_broker.py
